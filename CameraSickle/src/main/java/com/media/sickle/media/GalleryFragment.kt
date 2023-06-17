ecfcdf563a3222f08889fc6dbf78a3373fc3afa1
kpa <billkp@yeah.net>
2023-06-17 18:17:37 +0800

    完成相册预览

--- CameraSickle/src/main/java/com/media/sickle/media/GalleryFragment.kt
+++ CameraSickle/src/main/java/com/media/sickle/media/GalleryFragment.kt
@@ -18,17 +18,25 @@
 
 package com.media.sickle.media
 
+import android.media.MediaScannerConnection
+import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
-import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
+import com.media.sickle.R
 import com.media.sickle.databinding.FragmentGalleryBinding
+import com.media.sickle.utils.padWithDisplayCutout
+import com.media.sickle.utils.showImmersive
+import java.io.File
+import java.util.Locale
+
 val EXTENSION_WHITELIST = arrayOf("JPG")
 
 /**
@@ -36,53 +44,27 @@ val EXTENSION_WHITELIST = arrayOf("JPG")
  * status bar and navigation/system bar) with user interaction.
  */
 class GalleryFragment : Fragment() {
-    private val hideHandler = Handler(Looper.myLooper()!!)
-
-    @Suppress("InlinedApi")
-    private val hidePart2Runnable = Runnable {
-        // Delayed removal of status and navigation bar
-
-        // Note that some of these constants are new as of API 16 (Jelly Bean)
-        // and API 19 (KitKat). It is safe to use them, as they are inlined
-        // at compile-time and do nothing on earlier devices.
-        val flags =
-            View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                    View.SYSTEM_UI_FLAG_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-        activity?.window?.decorView?.systemUiVisibility = flags
-        (activity as? AppCompatActivity)?.supportActionBar?.hide()
-    }
-    private val showPart2Runnable = Runnable {
-        // Delayed display of UI elements
-        fullscreenContentControls?.visibility = View.VISIBLE
-    }
-    private var visible: Boolean = false
-    private val hideRunnable = Runnable { hide() }
-
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private val delayHideTouchListener = View.OnTouchListener { _, _ ->
-        if (AUTO_HIDE) {
-            delayedHide(AUTO_HIDE_DELAY_MILLIS)
-        }
-        false
-    }
-
-    private var dummyButton: Button? = null
-    private var fullscreenContent: View? = null
-    private var fullscreenContentControls: View? = null
 
     private var _binding: FragmentGalleryBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private val args: GalleryFragmentArgs by navArgs()
+
+    private lateinit var mediaList: MutableList<File>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //将其标记为保留片段，这样生命周期就不会在配置更改时重新启动
+        retainInstance = true
+
+        val rootDirectory = File(args.rootDirectory)
+        mediaList = rootDirectory.listFiles { file ->
+            EXTENSION_WHITELIST.contains(file.extension.toUpperCase(Locale.ROOT))
+        }?.sortedDescending()?.toMutableList() ?: mutableListOf()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -98,105 +80,72 @@ class GalleryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        visible = true
-
-        dummyButton = binding.dummyButton
-        fullscreenContent = binding.fullscreenContent
-        fullscreenContentControls = binding.fullscreenContentControls
-        // Set up the user interaction to manually show or hide the system UI.
-        fullscreenContent?.setOnClickListener { toggle() }
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        dummyButton?.setOnTouchListener(delayHideTouchListener)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        activity?.window?.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-
-        // Clear the systemUiVisibility flag
-        activity?.window?.decorView?.systemUiVisibility = 0
-        show()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        dummyButton = null
-        fullscreenContent = null
-        fullscreenContentControls = null
-    }
-
-    private fun toggle() {
-        if (visible) {
-            hide()
-        } else {
-            show()
+        if (mediaList.isEmpty()) {
+            binding.deleteButton.isEnabled = false
         }
+        binding.photoViewPager.apply {
+            offscreenPageLimit = 2
+            adapter = MediaPagerAdapter(childFragmentManager)
+        }
+
+        // Make sure that the cutout "safe area" avoids the screen notch if any
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            // Use extension method to pad "inside" view containing UI using display cutout's bounds
+            binding.cutoutSafeArea.padWithDisplayCutout()
+        }
+
+        // Handle back button press
+        binding.backButton.setOnClickListener {
+            Navigation.findNavController(requireActivity(), R.id.fragment_container).navigateUp()
+        }
+
+
+        // Handle delete button press
+        binding.deleteButton.setOnClickListener {
+
+            mediaList.getOrNull(binding.photoViewPager.currentItem)?.let { mediaFile ->
+                AlertDialog.Builder(view.context, android.R.style.Theme_Material_Dialog)
+                    .setTitle(getString(R.string.delete_title))
+                    .setMessage(getString(R.string.delete_dialog))
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setPositiveButton(android.R.string.yes) { _, _ ->
+
+                        // Delete current photo
+                        mediaFile.delete()
+
+                        // Send relevant broadcast to notify other apps of deletion
+                        MediaScannerConnection.scanFile(
+                            view.context, arrayOf(mediaFile.absolutePath), null, null
+                        )
+
+                        // Notify our view pager
+                        mediaList.removeAt(binding.photoViewPager.currentItem)
+                        binding.photoViewPager.adapter?.notifyDataSetChanged()
+
+                        // If all photos have been deleted, return to camera
+                        if (mediaList.isEmpty()) {
+                            Navigation.findNavController(requireActivity(), R.id.fragment_container)
+                                .navigateUp()
+                        }
+
+                    }
+
+                    .setNegativeButton(android.R.string.no, null)
+                    .create().showImmersive()
+            }
+        }
+
     }
 
-    private fun hide() {
-        // Hide UI first
-        fullscreenContentControls?.visibility = View.GONE
-        visible = false
+    inner class MediaPagerAdapter(fm: FragmentManager) :
+        FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+        override fun getCount(): Int = mediaList.size
 
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        hideHandler.removeCallbacks(showPart2Runnable)
-        hideHandler.postDelayed(hidePart2Runnable, UI_ANIMATION_DELAY.toLong())
-    }
+        override fun getItem(position: Int): Fragment =
+            PhotoFragment.create(mediaList.get(position))
 
-    @Suppress("InlinedApi")
-    private fun show() {
-        // Show the system bar
-        fullscreenContent?.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        visible = true
+        override fun getItemPosition(`object`: Any): Int = POSITION_NONE
 
-        // Schedule a runnable to display UI elements after a delay
-        hideHandler.removeCallbacks(hidePart2Runnable)
-        hideHandler.postDelayed(showPart2Runnable, UI_ANIMATION_DELAY.toLong())
-        (activity as? AppCompatActivity)?.supportActionBar?.show()
-    }
-
-    /**
-     * Schedules a call to hide() in [delayMillis], canceling any
-     * previously scheduled calls.
-     */
-    private fun delayedHide(delayMillis: Int) {
-        hideHandler.removeCallbacks(hideRunnable)
-        hideHandler.postDelayed(hideRunnable, delayMillis.toLong())
-    }
-
-    companion object {
-        /**
-         * Whether or not the system UI should be auto-hidden after
-         * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
-         */
-        private const val AUTO_HIDE = true
-
-        /**
-         * If [AUTO_HIDE] is set, the number of milliseconds to wait after
-         * user interaction before hiding the system UI.
-         */
-        private const val AUTO_HIDE_DELAY_MILLIS = 3000
-
-        /**
-         * Some older devices needs a small delay between UI widget updates
-         * and a change of the status and navigation bar.
-         */
-        private const val UI_ANIMATION_DELAY = 300
     }
 
     override fun onDestroyView() {
